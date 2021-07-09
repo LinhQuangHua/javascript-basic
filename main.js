3f8857371c02f54d66ed154431ac5c5c235be22b
QUANG HUA LINH <17DH111099@st.huflit.edu.vn>
2021-07-09 17:08:44 +0700

    Assignment operator

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-// Prefix and Postfix
+// Assignment operator
 
-var a = 5;
+var a = 1;
 
-// Prefix
-// Step 01: + 1 for a, a = a + 1 => a = 6
-// Step 02: return a after + 1
-var output = ++a;
-console.log(output);
+a += 2; // a = a + 2
 
-a = 5;
-// Postfix
-// Step 01: 'a copy', 'a copy' = 5
-// Step 02: - 1 for a, a = a - 1 => a = 6
-// Step 03: return 'a copy'
-var output = a++;
-console.log("output: " + output);
-console.log("a: " + a++);
+a -= 2; // a = a - 2
+
+a *= 2; // a = a * 2
+
+a /= 2; // a = a / 2
+
+a **= 2; // a = a ** 2 
+
+
