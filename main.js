1e5832fa7bf0d50970951af5bf88944c0c1fcad6
QUANG HUA LINH <17DH111099@st.huflit.edu.vn>
2021-07-09 15:50:39 +0700

    Arithmetic operators

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-setInterval(function () {
-    console.log(Math.random());
-}, 1000);
+// Arithmetic operators
+
+var a = 4, b = 2;
+
+var result = 0;
+
+result = a + b;
+
+result = a - b;
+
+result = a * b;
+
+result = a / b;
+
+
+result = a ** b;
+
+result = a % b;
+
+a++; // a + 1
+
+a--; // a - 1
