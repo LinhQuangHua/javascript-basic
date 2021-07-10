bb16bcd7c9b1621529369aef52137d8503e5defa
QUANG HUA LINH <17DH111099@st.huflit.edu.vn>
2021-07-10 13:53:10 +0700

    Compare operator - Part 02\

--- main.js
+++ main.js
@@ -1,52 +1,11 @@
-//1 - Primitive Data
+// Compare operator - Part 02
 
-// Number type
-var a = 1;
-var b = 1.5;
+var a = 1, b = 1, c = '1';
 
-console.log(typeof (a));
-// String type
-var name = "Pikachu";
-var nickName = "Linh 'Wang'";
+// ===
+console.log(a === b);
+console.log(a === c); // Compare value and data type
 
-// Boolean type
-var isOpen = true;
-
-// undefined type
-var age;
-
-// Null
-var isNull = null;
-console.log(typeof (isNull)); // typeof null is object
-// Symbol type
-var id = Symbol('id'); //unique
-var id2 = Symbol('id'); //unique
-
-console.log(id === id2); //false
-
-//2 - Complex Data
-
-// Array
-var myArray = [
-    'PHP',
-    'C#',
-    'Python',
-    'javascript'
-]
-
-console.log(myArray);
-
-// Function
-var myFunction = function () {
-    console.log("Helllo World");
-}
-
-myFunction();
-// Object
-var myObject = {
-    name: 'Linh Wang',
-    age: 20,
-    location: 'HCMC',
-}
-console.log(myObject);
-
+// !=== 
+console.log(a !== b);
+console.log(a !== c); // Compare value and data type
