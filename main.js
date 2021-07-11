cee68c65d93f5b4d06ebf6b00434ef942dddc98d
QUANG HUA LINH <17DH111099@st.huflit.edu.vn>
2021-07-11 11:52:24 +0700

    Number in Javascript

--- main.js
+++ main.js
@@ -1,45 +1,21 @@
-//Javascript string methods
+// Number in Javascript
 
-var myString = "Hello Pika Hello Togepi ";
+// Create
+var age = 18; // ==> type is number
+var PI = 3.14;
 
-// Length 
-console.log(myString.length);
+var otherNumber = new Number(9); // type is object
 
-// Find index 
-console.log(myString.indexOf('Hello'));  // No position start find
-console.log(myString.indexOf('Hello', 8)); // Have position start find
+// Check NaN
+var check01 = 20 / 'ABC';
+console.log(isNaN(check01)); // ==> true
+var check02 = 20 / 5;
+console.log(isNaN(check02)); // ==> flase
 
-// Search
-console.log(myString.search('Hello'));
+// To string
+console.log(age.toString());
 
-// Cut 
-console.log(myString.slice(6));
-console.log(myString.slice(6, 10));
-console.log(myString.slice(-7, -1));
-
-// Replace
-console.log(myString.replace('Hello', 'Hi')); // replace a first 'Hello' ==> 'Hi'
-console.log(myString.replace(/Hello/g, 'Hi')); // replace all 'Hello' ==> 'Hi'
-
-// Upper case
-console.log(myString.toUpperCase());
-
-// Upper case
-console.log(myString.toLowerCase());
-
-// Trim 
-var myName = "   Wang Hua   ";
-console.log(myName.trim()); // remove blank
-console.log(myName.length); //==> length is 14
-console.log(myName.trim().length); // ===> length is 8
-
-// Split
-var language = 'PHP, JS, Python';
-console.log(language.split(', '));
-var language2 = 'Ruby';
-console.log(language2.split(''));
-
-// Get a character by index
-var fullName = 'Wang Hua';
-console.log(fullName[0]); // not exists return undefined
-console.log(fullName.charAt(0)); // not exists return string
+// To fixed
+console.log(PI.toFixed()); // ==> return 3
+var money = 300.561487;
+console.log(money.toFixed(2)); // return 300.56
