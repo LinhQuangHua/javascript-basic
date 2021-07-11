740193178ea43c58cfce287f2462c5599bfa2962
QUANG HUA LINH <17DH111099@st.huflit.edu.vn>
2021-07-11 20:41:11 +0700

    Array in Javascript

--- main.js
+++ main.js
@@ -1,21 +1,41 @@
-// Number in Javascript
+// Array in Javascript
 
 // Create
-var age = 18; // ==> type is number
-var PI = 3.14;
+var language = [
+    'PHP',
+    'Python',
+    'C#',
+    'JavaScript',
+    undefined,
+    null,
+    function () {
 
-var otherNumber = new Number(9); // type is object
+    },
+    {},
+    123
+];
+console.log(language);
 
-// Check NaN
-var check01 = 20 / 'ABC';
-console.log(isNaN(check01)); // ==> true
-var check02 = 20 / 5;
-console.log(isNaN(check02)); // ==> flase
+var language2 = new Array(
+    'PHP',
+    'Python',
+    'C#',
+    'JavaScript',
+    undefined,
+    null,
+    function () {
 
-// To string
-console.log(age.toString());
+    },
+    {},
+    123
+)
 
-// To fixed
-console.log(PI.toFixed()); // ==> return 3
-var money = 300.561487;
-console.log(money.toFixed(2)); // return 300.56
+// Check type of array
+console.log(Array.isArray(language));
+
+// Length of array
+console.log(language.length);
+
+console.log(language[2]);
+
+
