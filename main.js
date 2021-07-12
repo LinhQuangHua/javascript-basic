61ffd9714f97e373e581e4946347d6117d427843
QUANG HUA LINH <17DH111099@st.huflit.edu.vn>
2021-07-12 12:23:32 +0700

    Object in Javascript

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
-// Types of function
+// Object in Javascript
 
-showMessage();
-// Declaration function
-function showMessage() {
-    console.log("Declaration function");
-}
-
-// Expression function
-
-var showMessage_02 = function () {
-    console.log("Expression function");
-}
-// Expression function - Callback
-setTimeout(function () {
-    console.log("Expression function - Callback");
-});
-// Expression function in object
-var myObject = {
-    myFunction: function () {
-        console.log("Expression function in object");
+// Create
+var room = 'classroom';
+var myInfo = {
+    name: 'Linh Wang', // key in bject is property
+    age: 20,
+    address: 'HCMC',
+    [room]: 'TH1715',
+    getName: function () {
+        return this.name;
     }
 }
+
+console.log(myInfo);
+console.log(myInfo.getName()); // function in object is method
+
+// Add key 'email' in Object
+myInfo.email = 'pikachu@gmail.com';
+//myInfo['email'] = 'pikachu@gmail.com'; // another way
+
+console.log(myInfo);
+
+// Get key 
+console.log(myInfo.name);
+//console.log(myInfo['name']);
+
+// Delete key
+delete myInfo.age;
+console.log(myInfo);
