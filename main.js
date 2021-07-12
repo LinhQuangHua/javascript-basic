01099e98cceef289cee1fe1a8703f04fc730ec17
QUANG HUA LINH <17DH111099@st.huflit.edu.vn>
2021-07-12 10:01:10 +0700

    Return in function

--- main.js
+++ main.js
@@ -1,39 +1,20 @@
-// Function in Javascript
+// Return in function
 
-// Create
-function showDialog() {
-    alert("Hello World!");
+// var isConfirm = confirm('Are you sure?');
+
+// console.log(isConfirm); 
+
+function Add(a, b) {
+    return a + b;
 }
-//showDialog();
 
-// Function with a parameter
-function writeLog(message) {
-    console.log(message);
+var result = Add(2, 8);
+
+console.log(result);
+
+function returnArray(a, b) {
+    return [a, b];
 }
-writeLog('Test message');
-writeLog(123);
-writeLog(['Python', 'JavaScript']);
 
-function writeLog_02(log, message) {
-    if (message) {
-        console.log(message);
-    }
-
-    if (log) {
-        console.log(log);
-    }
-
-    //console.log(arguments); ==> Out put array with all parameters in this function
-}
-writeLog_02("Log 01", "Log 02");
-
-// For loop
-function writeLog_03() {
-    var myString = '';
-    for (var param of arguments) {
-        //console.log(param);
-        myString += `${param} - `
-    }
-    console.log(myString);
-}
-writeLog_03("Log 01", "Log 02", "Log 03");
+var resultArray = returnArray(5, 7);
+console.log(resultArray);
