24618377f18a49f2319a1e7f1ffc79d7de8e9758
QUANG HUA LINH <17DH111099@st.huflit.edu.vn>
2021-07-13 22:19:41 +0700

    Javascript array methods - Part 02

--- main.js
+++ main.js
@@ -1,25 +1,71 @@
-// Nested loop
+// Javascript array methods - Part 02
 
-var myArray = [
-    [1, 2],
-    [3, 4],
-    [5, 6]
+var courses = [
+    {
+        id: 1,
+        name: 'Javascript',
+        coin: 250
+    },
+    {
+        id: 2,
+        name: 'HTML and CSS',
+        coin: 0
+    },
+    {
+        id: 3,
+        name: 'Python',
+        coin: 100
+    },
+    {
+        id: 4,
+        name: 'C#',
+        coin: 150
+    },
+    {
+        id: 5,
+        name: 'ReactJS',
+        coin: 200
+    },
+    {
+        id: 6,
+        name: 'Python',
+        coin: 300
+    }
 ];
 
-for (var i = 0; i < myArray.length; i++) {
-    for (var j = 0; j < myArray[i].length; j++) {
-        console.log(myArray[i][j]);
-    };
-};
+// for (const i in courses) {
+//     console.log(courses[i]);
+// }
 
-var language = [
-    ['PHP', 'Python'],
-    ['Java', 'JS'],
-    ['C#', 'C++']
-];
+// forEach
+courses.forEach(function (courses, index) {
+    console.log(index, courses);
+});
 
-for (var i = 0; i < language.length; i++) {
-    for (var j = 0; j < language[i].length; j++) {
-        console.log(language[i][j]);
-    };
-};
+// every
+var isFree = courses.every(function (courses, index) {
+    console.log(index);
+    return courses.coin === 0;
+});
+console.log(isFree);
+
+// some
+var isFree = courses.some(function (courses, index) {
+    console.log(index);
+    return courses.coin === 0;
+});
+console.log(isFree);
+
+// find
+var nameCourse = courses.find(function (courses, index) {
+    console.log(index);
+    return courses.name === 'Python'; // return first courses have name 'Python'
+});
+console.log(nameCourse);
+
+// filter
+var nameCourse = courses.filter(function (courses, index) {
+    console.log(index);
+    return courses.name === 'Python'; // return all courses have name 'Python'
+});
+console.log(nameCourse);
