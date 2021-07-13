d27eaaaaaf16d0a2fae77c341dcadeced3db3a02
QUANG HUA LINH <17DH111099@st.huflit.edu.vn>
2021-07-13 21:03:26 +0700

    While loop

--- main.js
+++ main.js
@@ -1,31 +1,21 @@
-// For/of loop
+// While loop
 
-var language = [
-    'PHP',
-    'Java',
-    'Python'
+var i = 0;
+
+while (i <= 10) {
+    i++;
+    console.log(i);
+}
+
+var a = 0;
+
+var myArray = [
+    'Javascript',
+    'Python',
+    'Java'
 ];
 
-for (var value of language) {
-    console.log(value);
-}
-
-var myString = 'Javascript';
-
-for (var value of myString) {
-    console.log(value);
-}
-
-// Use for/of loop object
-
-var myInfo = {
-    name: 'Wang Hua',
-    age: 18,
-    address: 'HCMC'
-};
-
-console.log(Object.values(myInfo));
-
-for (var value of Object.keys(myInfo)) {
-    console.log(value);
+while (a < myArray.length) {
+    console.log(myArray[a]);
+    a++;
 }
