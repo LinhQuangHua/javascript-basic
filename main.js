408bc9f0d1f79cf5944f3056aac5c62703d6eeac
QUANG HUA LINH <17DH111099@st.huflit.edu.vn>
2021-07-14 22:55:02 +0700

    Math object

--- main.js
+++ main.js
@@ -1,11 +1,41 @@
-// includes method
+// Math object
 
-var title = 'Hello world';
+// Math.PI 
+console.log(Math.PI);
 
-console.log(title.includes('Hello'));
+// Math.round()
+console.log(Math.round(1.3)); // return 1
+console.log(Math.round(1.5)); // return 2
 
-console.log(title.includes('Hello', 1)); // 1 is position search
+// Math.abs()
+console.log(Math.abs(-4)); //  |-4| = 4
 
-var course = ['PHP', 'JavaScript', 'C#'];
+// Math.ceil()
+console.log(Math.ceil(1.9));
+console.log(Math.ceil(1.00023));
 
-console.log(course.includes('C'));
+// Math.floor()
+console.log(Math.floor(1.9));
+console.log(Math.floor(1.00023));
+
+// Math.min()
+console.log(Math.min(-10, 50, 20, 10, 5, 7));
+
+// Math.max()
+console.log(Math.max(-10, 50, 20, 10, 5, 7));
+
+
+// Math.random()
+console.log(Math.random()); // Return number < 1
+console.log(Math.floor(Math.random() * 10))
+
+var random = Math.floor(Math.random() * 5);
+var bonus = [
+    '10 coin',
+    '20 coin',
+    '30 coin',
+    '40 coin',
+    '50 coin'
+];
+
+console.log(bonus[random]);
