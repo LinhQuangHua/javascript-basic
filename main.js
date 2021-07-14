c72c262f5e9d306f4b11a558a2e1c00726819291
QUANG HUA LINH <17DH111099@st.huflit.edu.vn>
2021-07-14 15:06:33 +0700

    includes method

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
-Array.prototype.reduce2 = function (callback, initialValue) {
-    let i = 0;
-    if (arguments.length < 2) {
-        i = 1;
-        initialValue = this[0];
-    }
-    for (; i < this.length; i++) {
-        initialValue = callback(initialValue, this[i], i, this); // this is array
-    }
-    return initialValue;
-}
-const number = [1, 2, 3, 4, 5]
+// includes method
 
-const result = number.reduce2(function (total, number) { return total + number }, 10);
-console.log(result);
+var title = 'Hello world';
+
+console.log(title.includes('Hello'));
+
+console.log(title.includes('Hello', 1)); // 1 is position search
+
+var course = ['PHP', 'JavaScript', 'C#'];
+
+console.log(course.includes('C'));
