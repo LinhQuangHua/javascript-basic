873b7f6c76ff1a1b5a44d0702325b68eb99aff3a
QUANG HUA LINH <17DH111099@st.huflit.edu.vn>
2021-07-14 14:20:46 +0700

    Logic of reduce()

--- main.js
+++ main.js
@@ -1,104 +1,15 @@
-// // Array reduce() method - Part02
-
-var courses = [
-    {
-        id: 1,
-        name: 'Javascript',
-        coin: 250
-    },
-    {
-        id: 2,
-        name: 'HTML and CSS',
-        coin: 0
-    },
-    {
-        id: 3,
-        name: 'Python',
-        coin: 100
-    },
-    {
-        id: 4,
-        name: 'C#',
-        coin: 150
-    },
-    {
-        id: 5,
-        name: 'ReactJS',
-        coin: 200
-    },
-    {
-        id: 6,
-        name: 'Python',
-        coin: 300
+Array.prototype.reduce2 = function (callback, initialValue) {
+    let i = 0;
+    if (arguments.length < 2) {
+        i = 1;
+        initialValue = this[0];
     }
-];
-
-// Total coin all courses
-var totalCoin = courses.reduce(function (total, courses) { return total + courses.coin }, 0);
-
-console.log(totalCoin);
-
-var number = [100, 400, 500, 200];
-
-var total = number.reduce(function (total, number) { return total + number });
-
-console.log(total);
-
-// Flat
-var depthArray = [1, 2, [3, 4], 5, 6, [7, 8, 9]];
-
-var flatArray = depthArray.reduce(function (flatOutput, depthItem) {
-    return flatOutput.concat(depthItem);
-}, []);
-
-console.log(flatArray);
-
-var topics = [
-    {
-        topic: 'front-end',
-        courses: [
-            {
-                id: 1,
-                title: 'HTML, CSS'
-            },
-            {
-                id: 2,
-                title: 'Javascript'
-            }
-        ]
-    },
-    {
-        topic: 'backend-end',
-        courses: [
-            {
-                id: 1,
-                title: 'PHP'
-            },
-            {
-                id: 2,
-                title: 'NodeJS'
-            }
-        ]
+    for (; i < this.length; i++) {
+        initialValue = callback(initialValue, this[i], i, this); // this is array
     }
-];
+    return initialValue;
+}
+const number = [1, 2, 3, 4, 5]
 
-var newArray = topics.reduce(function (course, topic) {
-    return course.concat(topic.courses)
-}, []);
-
-console.log(newArray);
-
-var html = newArray.map(function (course) {
-    return `
-        <div>
-            <h2>${course.title}</h2>
-            <p>${course.id}</p>
-        </div>
-    `;
-});
-
-console.log(html.join(''));
-
-
-
-
+const result = number.reduce2(function (total, number) { return total + number }, 10);
+console.log(result);
