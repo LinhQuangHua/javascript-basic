c922c1585337c6ab2b9f16461e758008a09ebc0e
QUANG HUA LINH <17DH111099@st.huflit.edu.vn>
2021-07-15 10:29:47 +0700

    Build filter menthod

--- main.js
+++ main.js
@@ -1,22 +1,37 @@
-// Build forEach menthod
+// Build filter menthod
 
-// var course = [
-//     'PHP',
-//     'JavaScript',
-//     'Python'
-// ];
+var course = [
+    {
+        name: 'Ruby',
+        coin: 100
+    },
+    {
+        name: 'Python',
+        coin: 300
+    },
+    {
+        name: 'C#',
+        coin: 200
+    }
+];
 
-var course = new Array(100);
-course.push('PHP', 'JavaScript');
-
-Array.prototype.forEach2 = function (callback) {
+Array.prototype.filter2 = function (callback) {
+    output = [];
     for (var index in this) {
         if (this.hasOwnProperty(index)) {
-            callback(this[index], index, this);
+            var result = callback(this[index], index, this);
+            if (result) {
+                output.push(this[index]);
+            }
         }
     }
+    return output;
 };
 
-course.forEach2(function (course, index, array) {
-    console.log(course, index, array);
-})
+var filterCourses = course.filter2(function (course) {
+    return course.coin > 100;
+});
+console.log(filterCourses);
+
+
+
