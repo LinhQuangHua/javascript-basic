7d6b14785088252f32acb61cd756ba78b198bd48
QUANG HUA LINH <17DH111099@st.huflit.edu.vn>
2021-07-15 23:38:30 +0700

    Get element methods - Part01

--- main.js
+++ main.js
@@ -1,39 +1,38 @@
-// Build some menthod
+// HTML DOM
 
-var course = [
-    {
-        name: 'Ruby',
-        coin: 700,
-        isFinish: true
-    },
-    {
-        name: 'Python',
-        coin: 650,
-        isFinish: true
-    },
-    {
-        name: 'C#',
-        coin: 500,
-        isFinish: true
-    }
-];
+/**
+ * 1. Elements: Get by ID, class, tag, CSS selector, HTML collection
+ * 2. Attribute:
+ * 3. Text: 
+ */
 
-Array.prototype.every2 = function (callback) {
-    for (var index in this) {
-        if (this.hasOwnProperty(index)) {
-            var result = callback(this[index], index, this);
-            if (!result) {
-                return false;
-            }
-        }
-    }
-    return true;
-};
+var getById = document.getElementById('heading');
+console.log(getById);
 
-var filterCourses = course.every2(function (course) {
-    return course.isFinish;
-});
-console.log(filterCourses);
+var getClassName = document.getElementsByClassName('heading');
+console.log(getClassName);
+
+var getTagName = document.getElementsByTagName('h1');
+console.log(getTagName);
+
+var getCssSelector = document.querySelector('.heading');
+console.log(getCssSelector);
+
+var getHeading01 = document.querySelector('.box .heading-2:first-child');
+console.log(getHeading01);
+
+var getHeading02 = document.querySelector('.box .heading-2:nth-child(2)');
+console.log(getHeading02);
+
+var getAllHeading = document.querySelectorAll('.box .heading-2');
+console.log(getAllHeading);
+console.log(getAllHeading[0]);
+
+var htmlCollection = document.forms.form01;
+// var htmlCollection = document.forms['form01'];
+console.log(htmlCollection);
+
+console.log(document.anchors);
 
 
 
