53a1e250e9a57105eab7b32a1d14a23a3bd7f7cf
QUANG HUA LINH <17DH111099@st.huflit.edu.vn>
2021-07-18 10:40:30 +0700

    Promise(sync, async)

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
-// JSON(JavaScript Object Notation)
+// Promise(sync, async)
 
-// Stringify: JavaScript type to JSON
-// Parse: JSON to JavaScript type
+// Async: setTimeout, setInterval, fetch, XMLHttpRequest, request animation frame
 
-var a = '1'; // number in JSON
-console.log(typeof JSON.parse(a));
+var promise = new Promise(
+    //Executor
+    function (resolve, reject) {
+        resolve(
+            {
+                id: '1',
+                name: 'Javascript'
+            }
+        );
+        // reject('Not found!');
+    }
+);
 
-var b = 'true'; // boolean in JSON
-console.log(typeof JSON.parse(b));
+promise
+    .then(function (courses) {
+        console.log(courses);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+    .finally(function () {
+        console.log('Done!');
+    });
 
-var c = '"Hello"'; // string in JSON
-console.log(typeof JSON.parse(c));
-
-var json = '{"name":"Wang Hua", "age":18}';
-var object = JSON.parse(json);
-console.log(typeof object);
-
-console.log(JSON.stringify({
-    name: 'Wang',
-    age: 18,
-}));
